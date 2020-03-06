8205042599ccbff3d2e1634ddf538657c51568e2
Éric Férole <eferole@pixelcircus.ca>
2020-03-05 19:22:11 -0500

    Model Follow

---
+++ app/models/beer_location.rb
@@ -0,0 +1,2 @@
+class BeerLocation < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_001814) do
+ActiveRecord::Schema.define(version: 2020_03_06_002138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "beer_locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "beers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follows", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
