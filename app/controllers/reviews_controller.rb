4448f7184095c1f2c6d259826817c414221f36b0
kirill ginko <kirillginko@gmail.com>
2020-03-10 13:50:21 -0400

    added form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @beer = Beer.find(params[:beer_id])
+    @review = Review.new(review_params)
+    @review.beer = @beer
+    @review.user = current_user
+    if @beer.save
+      redirect_to beer_path(@beer)
+    else
+      render 'beers/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :likes, :stars)
+  end
 end
