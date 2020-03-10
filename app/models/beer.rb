d1e612ee21857e16195a586cec3b6aae2b9d53bf
kirill ginko <kirillginko@gmail.com>
2020-03-09 23:07:06 -0400

    updated index

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
+  has_one_attached :photo
+  has_one_attached :avatar
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_160848) do
+ActiveRecord::Schema.define(version: 2020_03_10_013301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_160848) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_beers_on_user_id"
   end
 
@@ -94,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_160848) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.string "name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
