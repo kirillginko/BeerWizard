46f70e99d421b2277954f4dfd1f72c9678450adf
kirill ginko <kirillginko@gmail.com>
2020-03-10 20:12:57 -0400

    views

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :beer
   validates :description, length: { minimum: 20}
+  validates :stars, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5]}
 end
