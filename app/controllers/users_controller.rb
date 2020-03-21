eddefc59cc9512a7e1d78322d8c4fe100d2b26a6
kirill ginko <kirillginko@gmail.com>
2020-03-21 16:16:46 -0400

    users searchbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
 
   def index
+    if params[:query].present?
+      @userquery =  "#" + params[:query]
+      user_query = " \
+      users.name @@ :query \
+"
+      @users = User.where(user_query, query: "%#{params[:query]}%")
+    else
     @users = User.all.order("created_at DESC")
   end
+end
 
 
   def show
