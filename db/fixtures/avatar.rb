4218ad87b886780a7d296900769357c4f517c7f2
Guillaume Lenzinger <glenzinger@gmail.com>
2020-03-24 23:26:20 -0400

    seed avatar in progress

--- db/fixtures/avatar.rb
+++ db/fixtures/avatar.rb
@@ -1,8 +1,16 @@
 require 'faker'
 
-10.times do
-avatarArray = [Faker::Avatar.image]
-end
-new_pic = User.find(2)
-new_pic.avatar.attach(io: avatarArray[0], filename: 'beer2.png', content_type: "image/png")
-new_pic.save!
+#10.times do
+#avatarArray = [Faker::Avatar.image]
+#end
+#p avatarArray
+#new_pic = User.find(2)
+#new_pic.avatar.attach(io: Faker::Avatar.image, filename: 'beer2.png', content_type: "image/png")
+#new_pic.save!
+
+file = URI.open(Faker::Avatar.image)
+  new_image = User.find(2)
+  new_image.avatar.attach(io: file, filename: 'Melissa', content_type: "image/png")
+  new_image.save!
+
+p Faker::Avatar.image
