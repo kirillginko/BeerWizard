a2bd94b76cff9c289e7c12e1ad1602325430b692
Éric Férole <eferole@pixelcircus.ca>
2020-03-30 21:45:41 -0400

    Fix navbar

---
+++ db/fixtures/beer-images.rb
@@ -0,0 +1,49 @@
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585611843/BeerWizard%20Beers/sir-wallace_dzgel0.png")
+#   beer2 = Beer.find(2)
+#   beer2.avatar.attach(io: file, filename: 'Sir Wallace', content_type: "image/png")
+#   beer2.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+#   beer21 = Beer.find(21)
+#   beer21.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+#   beer21.save!
+
+file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+  beer22 = Beer.find(22)
+  beer22.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+  beer22.save!
+
+file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585611305/BeerWizard%20Beers/nano-ipa_jxf0iv.png")
+  beer23 = Beer.find(23)
+  beer23.avatar.attach(io: file, filename: 'Nano IPA', content_type: "image/png")
+  beer23.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+#   beer24 = Beer.find(24)
+#   beer24.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+#   beer24.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+#   beer25 = Beer.find(25)
+#   beer25.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+#   beer25.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+#   beer26 = Beer.find(26)
+#   beer26.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+#   beer26.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+#   beer26 = Beer.find(26)
+#   beer26.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+#   beer26.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585607461/BeerWizard%20Beers/zizen_pils_hlavs5.png")
+#   beer26 = Beer.find(26)
+#   beer26.avatar.attach(io: file, filename: 'Zizen Pils', content_type: "image/png")
+#   beer26.save!
+
+# file = URI.open("https://res.cloudinary.com/dfnvi7ruc/image/upload/v1585608987/BeerWizard%20Beers/new-wave-milkshake_ufhurq.png")
+#   beer29 = Beer.find(29)
+#   beer29.avatar.attach(io: file, filename: 'New Wave Milkshake', content_type: "image/png")
+#   beer29.save!
