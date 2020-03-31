01f69bf92dc43d3a2d2fb17eba29426c471f51f5
kirill ginko <kirillginko@gmail.com>
2020-03-31 14:46:31 -0400

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index]
 
   def home
-
+    if params[:query].present?
+    @beers = Beer.search_title(params[:query])
+    else
+    @beers = Beer.all.order("name ASC")
   end
+  @users = User.all.order("name Asc")
+end
 end
