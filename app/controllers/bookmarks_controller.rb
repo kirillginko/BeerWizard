092b159b574978b2c76d99a20d1dbb684f14d30d
kirill ginko <kirillginko@gmail.com>
2020-04-04 10:16:55 -0400

    home fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,9 +5,11 @@ class BookmarksController < ApplicationController
     if bookmark == []
       # Create Bookmark
       Bookmark.create(beer: Beer.find(params[:beer]), user: current_user)
+      flash[:notice] = "Bookmark Saved!"
       @bookmark_exists = true
     else
       # Delete Bookmark
+      flash[:error] = "Bookmark Deleted!"
       bookmark.destroy_all
       @bookmark_exists = false
     end
