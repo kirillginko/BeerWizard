6bdcb17f193a6aca3b7930b60566d7f9e3535178
kirill ginko <kirillginko@gmail.com>
2020-04-04 11:12:04 -0400

    bookmark fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,15 +1,15 @@
 class BookmarksController < ApplicationController
   respond_to :js, :json, :html
   def update
-    bookmark = Bookmark.where(beer: Beer.find(params[:beer]), user: current_user)
+    @beer = Beer.find(params[:beer])
+    @user = current_user
+    bookmark = Bookmark.where(beer: @beer,user: @user)
     if bookmark == []
       # Create Bookmark
       Bookmark.create(beer: Beer.find(params[:beer]), user: current_user)
-      flash[:notice] = "Bookmark Saved!"
       @bookmark_exists = true
     else
       # Delete Bookmark
-      flash[:error] = "Bookmark Deleted!"
       bookmark.destroy_all
       @bookmark_exists = false
     end
